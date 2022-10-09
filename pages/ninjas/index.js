38e6a2a6eb37ac923370d28f8aa9eb6f158d7dc7
Tanim Ismat <tanim.ismat@gmail.com>
2022-10-09 14:23:04 +0600

    styles

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,5 @@
+import styles from "../../styles/Ninjas.module.css";
+
 export const getStaticProps = async () => {
 	const res = await fetch("https://jsonplaceholder.typicode.com/users");
 	const data = await res.json();
@@ -14,7 +16,7 @@ const Ninjas = ({ ninjas }) => {
 			<h1>All ninjas</h1>
 			{ninjas.map((ninja) => (
 				<div key={ninja.id}>
-					<a>
+					<a className={styles.single}>
 						<h3>{ninja.name}</h3>
 					</a>
 				</div>
