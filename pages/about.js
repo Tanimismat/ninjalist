bacf879d5faecfea0653224d6fba604232de8c52
Tanim Ismat <tanim.ismat@gmail.com>
2022-10-09 11:34:20 +0600

    using metadata

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,33 @@
+import Head from "next/head";
+
 const About = () => {
 	return (
-		<div>
-			<h1>About</h1>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
-				eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
-				inventore voluptatem unde. Laudantium placeat veniam similique
-				recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
-				saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
-				velit enim eum! Earum eius iusto veniam.
-			</p>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
-				eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
-				inventore voluptatem unde. Laudantium placeat veniam similique
-				recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
-				saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
-				velit enim eum! Earum eius iusto veniam.
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				{/* Giving search engine more info using meta keyword */}
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
+					eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
+					inventore voluptatem unde. Laudantium placeat veniam similique
+					recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
+					saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
+					velit enim eum! Earum eius iusto veniam.
+				</p>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
+					eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
+					inventore voluptatem unde. Laudantium placeat veniam similique
+					recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
+					saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
+					velit enim eum! Earum eius iusto veniam.
+				</p>
+			</div>
+		</>
 	);
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,27 +7,34 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
-			<h1 className={styles.title}>Homepage</h1>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
-				eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
-				inventore voluptatem unde. Laudantium placeat veniam similique
-				recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
-				saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
-				velit enim eum! Earum eius iusto veniam.
-			</p>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
-				eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
-				inventore voluptatem unde. Laudantium placeat veniam similique
-				recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
-				saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
-				velit enim eum! Earum eius iusto veniam.
-			</p>
-			<Link href="/ninjas">
-				<a className={styles.btn}>See all ninjas</a>
-			</Link>
-		</div>
+		<>
+			<Head>
+				<title>Ninja List | Home</title>
+				{/* Giving search engine more info using meta keyword */}
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1 className={styles.title}>Homepage</h1>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
+					eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
+					inventore voluptatem unde. Laudantium placeat veniam similique
+					recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
+					saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
+					velit enim eum! Earum eius iusto veniam.
+				</p>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi beatae
+					eius, quo suscipit necessitatibus deleniti impedit sunt aperiam,
+					inventore voluptatem unde. Laudantium placeat veniam similique
+					recusandae numquam aperiam ea fugiat, aliquam facere blanditiis quo
+					saepe expedita totam vero dolore cupiditate ipsum nihil, architecto
+					velit enim eum! Earum eius iusto veniam.
+				</p>
+				<Link href="/ninjas">
+					<a className={styles.btn}>See all ninjas</a>
+				</Link>
+			</div>
+		</>
 	);
 }
