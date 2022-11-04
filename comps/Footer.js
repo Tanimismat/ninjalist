83a5b26d6399e1993c0d20834ae71d4963671437
Tanim Ismat <tanim.ismat@gmail.com>
2022-11-04 13:00:24 +0600

    styled-icons

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,9 +1,12 @@
+import { Eye } from "@styled-icons/bootstrap";
+
 const Footer = () => {
 	const year = new Date().getFullYear();
 
 	return (
 		<footer>
 			<p>Copyright {year} Ninja List</p>
+			<Eye width={16} height={16} />
 		</footer>
 	);
 };
